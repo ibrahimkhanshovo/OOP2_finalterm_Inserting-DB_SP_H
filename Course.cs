298fe7ccf8c5f13d3a6436b4c7d878c9ba8df5a0
Ibrahim Khan Shovo <78006923+ibrahimkhanshovo@users.noreply.github.com>
2021-04-14 00:33:19 +0600

    Inserting into Database

---
+++ Course.cs
@@ -0,0 +1,19 @@
+﻿
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Insert_into_db
+    {
+        class COURSE
+        {
+            public int id { get; set; }
+            public string Course_Name { get; set; }
+            public string Course_Code { get; set; }
+        }
+    }
+}
+    }
+}
---
+++ Database.cs
@@ -0,0 +1,25 @@
+﻿
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+            using System.Data.SqlClient;
+
+namespace Insert_into_db
+    {
+        class Database
+        {
+            public static SqlConnection conn;
+            public static SqlConnection ConnectDB()
+            {
+                string connString = @"Server=IBRAHIM\SQLEXPRESS;Database=course;Integrated Security=true";
+                SqlConnection conn = new SqlConnection(connString);
+                return conn;
+            }
+        }
+    }
+© 2021 GitHub, Inc.
+        }
+    }
+}
